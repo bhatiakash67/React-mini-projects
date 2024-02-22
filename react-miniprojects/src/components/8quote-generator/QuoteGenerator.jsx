5c84fc26cb0cb8ca6823a7c6a693bdbdbc40b623
bhatiakash67 <akashbhati67@gmail.com>
2024-02-22 23:44:53 +0530

    quote generator
    random quote generator made by fetching api data. Implemented exception handling - try, catch and finally. Used useEffect to perform side effects.

--- react-miniprojects/src/components/8quote-generator/QuoteGenerator.jsx
+++ react-miniprojects/src/components/8quote-generator/QuoteGenerator.jsx
@@ -1,17 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function QuoteGenerator() {
-    return(
+
+    const [quote, setQuote] = useState("")
+    const [author, setAuthor] = useState("")
+    const [loading, setLoading] = useState(true);
+
+    const getNewQuote = async () => {
+        
+        try {
+            setLoading(true)
+            const response = await fetch("https://api.quotable.io/quotes/random?limit=25")
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch quote.")
+            }
+
+            const quotes = await response.json();
+            const { content, author } = quotes[0]
+            setQuote(content)
+            setAuthor(author)
+        }
+
+        catch (error) {
+            console.error("Error fetching quote:", error)
+        }
+        finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        getNewQuote()
+    }, [])
+
+    return (
         <div className='quote-body w-screen h-screen bg-red-200 flex items-center justify-center'>
             <div className='quote-innerbody flex flex-col justify-between items-center border-black rounded-lg w-[50%] border-[10px] bg-red-100 shadow-xl'>
-                
-                <p className='quote-header mt-5 text-5xl'>Quote of the Day</p>
-                <div className='quote-content flex flex-col justify-center items-center text-4xl ml-5 mr-5'><h3 className='quote mt-10'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit illum, pariatur at eligendi vitae nisi cumque tempora ratione eius recusandae.</h3>
-                <h3 className='quote-writer mt-3 mb-10'>- Lorem, ipsum.</h3></div>
-                
-                <button className='bg-red-500 rounded-2xl p-5 mb-5'>Click for a new Quote</button>
+
+                <p className='quote-header mt-5 text-5xl bg-black rounded-lg text-white p-5'>Quote of the Day</p>
+
+                {loading ? (
+                    <p className='m-10 text-3xl'>Loading quotes. Please wait.</p> 
+                    
+                    ) : (
+
+                    <div className='quote-content flex flex-col justify-center items-center text-4xl ml-5 mr-5'>
+                        <h3 className='quote mt-10'>{quote}</h3>
+                        <h3 className='quote-writer mt-3 mb-10 font-bold '>- {author}</h3>
+                    </div>
+                )}
+
+                <button className='bg-red-500 rounded-2xl p-5 mb-5'
+                    onClick={() => getNewQuote()}
+                >Click for a new Quote</button>
             </div>
-        
         </div>
     )
 

1449b2d25d337f43ea30822cd3e5497578bcd63c
bhatiakash67 <akashbhati67@gmail.com>
2024-02-27 14:00:55 +0530

    toggle single/multiple accordion from a button

--- react-miniprojects/src/components/9-Accordion/accordion-with-array/ToggleAccordionWithArray.jsx
+++ react-miniprojects/src/components/9-Accordion/accordion-with-array/ToggleAccordionWithArray.jsx
@@ -4,53 +4,75 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 
 function ToggleAccordionWithArray() {
+    const [openItems, setOpenItems] = useState([])
+    const [toggle, setToggle] = useState(false)
 
-    const [selectedId, setSelectedId] = useState(null)
+    function toggleAccordion() {
+        setToggle((prevToggle) => !prevToggle)
+    }
+
+    function singleAccordion(id) {
+        setOpenItems((prevOpenItems) => (prevOpenItems === id ? [] : id))
+    }
 
     function multipleAccordion(id) {
-        setSelectedId((prevSelectedId) => (prevSelectedId === id ? null : id))
+        setOpenItems((prevOpenItems) => {
+            if (prevOpenItems.includes(id)) {
+                return prevOpenItems.filter((itemId) => itemId !== id)
+            }
+            else {
+                return [...prevOpenItems, id]
+            }
+        })
     }
+    const handleAccordionClick = toggle ? multipleAccordion : singleAccordion;
 
     return (
         <div className="body flex-col justify-center items-center h-screen">
-
-            <div className='flex justify-center mt-52 w-screen text-white' ><button
-                className='rounded-2xl p-5 bg-black'>Click to toggle Accordion</button>
+            <div className='flex justify-center mt-52 w-screen text-white'>
+                <button
+                    onClick={toggleAccordion}
+                    className='rounded-2xl p-5 bg-black text-2xl hover:text-yellow-500'>
+                    {toggle ? "Click for Single Accordion" : "Click for Multiple Accordion"}
+                </button>
             </div>
+            
             <div className='flex justify-center items-center mt-9'>
-                <div className="innerbody p-5 border-xl w-1/2 bg-red-200 rounded-2xl">
-                    <h1 className='text-3xl font-bold flex justify-center'>FAQ</h1>
+                <div className="innerbody p-5 border-xl w-1/2 bg-gray-200 rounded-2xl">
+                    <h1 className='text-5xl font-bold flex p-5'>Frequently Asked Questions</h1>
                     {data && data.length !== 0 ? (
                         <div>
                             {data.map((item) => (
-                                <div className='q-n-a m-2 h-1/3 bg-green-100 rounded-2xl p-5 cursor-pointer'
+                                <div className='q-n-a m-2 h-1/3 bg-gray-100 rounded-2xl p-5 cursor-pointer'
                                     key={item.id}
-                                    onClick={() => multipleAccordion(item.id)}>
+                                    onClick={() =>
+                                        handleAccordionClick(item.id)
+                                    }>
                                     <div className='questions flex justify-between items-center text-3xl font-bold'>
                                         {item.question}
                                         <button
-                                            className='bg-black text-white rounded-full p-3'>
-                                            {selectedId === item.id ? <FontAwesomeIcon icon={faMinus} /> : <FontAwesomeIcon icon={faPlus} />}
+                                            className='text-black p-3'>
+                                            {
+                                                openItems.includes(item.id)
+                                                    ? <FontAwesomeIcon icon={faMinus} /> : <FontAwesomeIcon icon={faPlus} />}
                                         </button>
                                     </div>
-                                    {selectedId === item.id && (
-                                        <div className="answers p-5 text-3xl ">
-                                            {item.answer}
-                                        </div>
-                                    )}
+                                    {
+                                        openItems.includes(item.id) &&
+                                        (
+                                            <div className="answers p-5 text-3xl ">
+                                                {item.answer}
+                                            </div>
+                                        )}
                                 </div>
                             ))}
-
                         </div>
                     ) : (
-
-                        <div>No data found.</div>)
+                        <div className='flex justify-center items-center font-bold text-5xl m-5'>No data found.</div>)
                     }
                 </div>
             </div>
         </div >
-
     )
 }
-
 export default ToggleAccordionWithArray
